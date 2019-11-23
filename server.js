7cb1f93d56991dbe476742594c741f5d6ee220e1
Eddie <edfacioli@gmail.com>
2019-11-23 18:13:43 -0300

    updates

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ require("dotenv").config();
 const app = require('./src/config/config-express');
 
 app.listen(process.env.PORT, function () {
-    console.log(`Servidor rodando na porta 3001`);
+    console.log('Servidor rodando na porta ' + process.env.PORT);
 });
